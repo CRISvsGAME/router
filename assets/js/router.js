5bd37cb85c6aee91a70e2fca052586d406581395
cris <cris@crisvsgame.com>
2024-09-05 11:56:15 +0100

    Router 1.0.1
    Modified Callback Functions
    Added Props to Callback Functions

--- assets/js/router.js
+++ assets/js/router.js
@@ -1,5 +1,5 @@
 /*!
- * Router 1.0.0 by @CRISvsGAME - https://crisvsgame.com
+ * Router 1.0.1 by @CRISvsGAME - https://crisvsgame.com
  * Repository - https://github.com/CRISvsGAME/router.git
  * License - https://crisvsgame.com/license (MIT License)
  * Copyright 2024 @CRISvsGAME
@@ -9,20 +9,29 @@
  * Options for configuring the Router.
  * @typedef {Object} RouterOptions
  * @property {string[]} [sectionIds=[]] - Array of section IDs.
- * @property {string} [defaultSection=''] - Default section ID to display initially.
+ * @property {string} [defaultSection=null] - Default section ID to display initially.
  * @property {number} [initialDelay=0] - Initial delay in milliseconds before starting.
  * @property {number} [sectionDelay=0] - Delay in milliseconds before changing the section.
- * @property {boolean} [saveState=true] - Save the active section to local storage.
- * @property {function} [refreshPageCallback=null] - Callback function for page refreshes,
- * containing the saved section ID.
- * @property {function} [buttonClickCallback=null] - Callback function for button clicks,
- * containing the event and the current section ID.
+ * @property {boolean} [saveState=true] - Save the active section to session storage.
+ * @property {function(RouterCallbackProps): void} [pageLoadCallback=null] - Callback function for page loads,
+ * containing the default, active, saved section IDs.
+ * @property {function(RouterCallbackProps): void} [buttonClickCallback=null] - Callback function for button clicks,
+ * containing the event and the default, active, saved section IDs.
+ */
+
+/**
+ * Callback props for pageLoadCallback and buttonClickCallback.
+ * @typedef {Object} RouterCallbackProps
+ * @property {Event} [event] - Event object (for buttonClickCallback).
+ * @property {string} [defaultSection] - Default section ID.
+ * @property {string} [activeSection] - Currently active section ID.
+ * @property {string} [savedSection] - Saved section ID from sessionStorage (if any).
  */
 
 class Router {
     /**
      * Creates a new Router instance.
-     * @param {string} [containerId='router'] - The ID of the container element.
+     * @param {string} [containerId='router'] - ID of the container element.
      * @param {RouterOptions} [options={}] - Configuration options for the Router.
      */
     constructor(containerId = 'router', options = {}) {
@@ -30,16 +39,16 @@ class Router {
         if (!this.container) {
             throw new Error(`Container element with ID "${containerId}" not found`);
         }
-        this.activeSection = '';
-        this.savedSection = localStorage.getItem('savedSection') || '';
+        this.activeSection = null;
+        this.savedSection = sessionStorage.getItem('savedSection') || null;
         this.sectionTimeout = null;
         this.options = {
             sectionIds: [],
-            defaultSection: '',
+            defaultSection: null,
             initialDelay: 0,
             sectionDelay: 0,
             saveState: true,
-            refreshPageCallback: null,
+            pageLoadCallback: null,
             buttonClickCallback: null,
             ...options,
         };
@@ -55,16 +64,25 @@ class Router {
             this.buttons[button.dataset.section].push(button);
             button.addEventListener('click', (event) => {
                 if (typeof this.options.buttonClickCallback === 'function') {
-                    this.options.buttonClickCallback(event, this.activeSection);
+                    this.options.buttonClickCallback({
+                        event: event,
+                        defaultSection: this.options.defaultSection,
+                        activeSection: this.activeSection,
+                        savedSection: this.savedSection,
+                    });
                 }
                 this.#changeSection(button.dataset.section, this.options.sectionDelay);
             });
         });
+        if (typeof this.options.pageLoadCallback === 'function') {
+            this.options.pageLoadCallback({
+                defaultSection: this.options.defaultSection,
+                activeSection: this.activeSection,
+                savedSection: this.savedSection,
+            });
+        }
         if (this.options.saveState && this.savedSection) {
             this.#changeSection(this.savedSection, this.options.initialDelay);
-            if (typeof this.options.refreshPageCallback === 'function') {
-                this.options.refreshPageCallback(this.savedSection);
-            }
         } else {
             this.#changeSection(this.options.defaultSection, this.options.initialDelay);
         }
@@ -72,12 +90,12 @@ class Router {
 
     /**
      * Changes the active section with delay if specified.
-     * @param {string} section - The section to activate.
+     * @param {string} section - Section to activate.
      * @param {number} [delay=0] - Delay in milliseconds before changing the section.
      * @private
      */
     #changeSection(section, delay = 0) {
-        if (section === this.activeSection) {
+        if (section === this.activeSection || !this.sections[section]) {
             return;
         }
         clearTimeout(this.sectionTimeout);
@@ -92,7 +110,7 @@ class Router {
 
     /**
      * Handles the actual change of sections.
-     * @param {string} section - The section to activate.
+     * @param {string} section - Section to activate.
      * @private
      */
     #handleSection(section) {
@@ -104,7 +122,7 @@ class Router {
         if (newSection) newSection.classList.add('active');
         if (oldButtons) oldButtons.forEach((button) => button.classList.remove('active'));
         if (newButtons) newButtons.forEach((button) => button.classList.add('active'));
-        if (this.options.saveState) localStorage.setItem('savedSection', section);
+        if (this.options.saveState) sessionStorage.setItem('savedSection', section);
         this.activeSection = section;
     }
 }
